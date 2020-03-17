b96ffa29d122f6c5bb17d2b32e4b957aa8d3df9b
Eric Garcia <egarcia1868@gmail.com>
2020-03-16 21:47:25 -0500

    adding controller & starting View

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ const orm = require("../config/orm");
 
 const burger = {
   selectAll: (cb) => {
-    orm.selectAll("burgers", (res) => {
+    orm.selectAll("burgers", res => {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   insertOne: (cols, vals, cb) => {
-    orm.insertOne("burgers", cols, vals, (res) => {
+    orm.insertOne("burgers", cols, vals, res => {
       cb(res);
     });
   },
   updateOne: (objColVals, condition, cb) => {
-    orm.updateOne("burgers", objColVals, condition, (res) => {
+    orm.updateOne("burgers", objColVals, condition, res => {
       cb(res);
     });
   }
